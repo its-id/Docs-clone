c9a1332884a21a082fd269f09fa3c4f42155751b
Indrakant Dana <ik.dana24@gmail.com>
2023-05-21 19:01:25 +0530

    Add Tailwind & React-Draft

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useState, Component } from "react";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
   return (
     <div className="App">
-
-      
+      <Navbar />
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={(e) => setEditorState(e)}
+      />
     </div>
   );
 }
